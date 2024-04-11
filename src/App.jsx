769ec1eaf94f18968948ba166e2552de9fc40cfa
Max de Vocht <maxdevocht@hotmail.com>
2024-04-11 15:07:52 +0200

    Added Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 import './App.css'
 
 export default function App() {
 
   return (
-    <></>
+    <Navbar />
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <i class="ai-location"></i>
+                <h1 className="logo--name">WeBarelyWork</h1>
+            </div>
+            <a href="#" className="nav--menu"><i class="ai-text-align-justified"></i></a>
+        </nav>
+    )
+}
